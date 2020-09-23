8f56a5c5c509214d1460982e9f67f608ae5315a7
manasomali <matheus.marques_96@hotmail.com>
2020-09-23 15:28:56 -0300

    contrutor da classe Livro

--- src/main/kotlin/predesafio/Livro.kt
+++ src/main/kotlin/predesafio/Livro.kt
@@ -1,4 +1,10 @@
 package predesafio
 
-class Livro {
+class Livro(var codigo: Int,
+            var titulo: String,
+            var autor: String,
+            var anoDeLancamento: Int,
+            var quantidade: Int,
+            var preco: Double) {
+    
 }
