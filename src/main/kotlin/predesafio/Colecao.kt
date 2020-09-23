6fc38fe446249cf43ab81379abe27b5d6b626331
MariaAA90 <ma_armelin@hotmail.com>
2020-09-23 16:23:21 -0300

    Novas mudanças

--- src/main/kotlin/predesafio/Colecao.kt
+++ src/main/kotlin/predesafio/Colecao.kt
@@ -1,6 +1,15 @@
 package predesafio
 
-class Colecao(var codigo: Int, var preco: Double, var livros: MutableList<Int>, var descricao: String) : IProduto {
-    var quantidade = 0
+class Colecao(override var codigo: Int, var livros: MutableList<Int>, var descricao: String) : IProduto {
+    override var quantidade = 0
+    override var preco = 0.0
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is IProduto) return false
+        if (codigo != other.codigo) return false
+
+        return true
+    }
 
 }
