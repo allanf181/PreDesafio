fc284c8348845d8c0e71a1cf3c1235d9bc1f4265
manasomali <matheus.marques_96@hotmail.com>
2020-09-23 16:15:51 -0300

    atualização 4 classe Livros

--- src/main/kotlin/predesafio/Livro.kt
+++ src/main/kotlin/predesafio/Livro.kt
@@ -3,12 +3,14 @@ package predesafio
 class Livro(override var codigo: Int,
             var titulo: String,
             var autor: String,
-            var anoDeLancamento: Int,
-            override var preco: Double,
-            override var quantidade: Int): IProduto {
+            var anoDeLancamento: Int): IProduto {
+
+    override var preco = 0.0
+    override var quantidade = 0
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other !is Livro) return false
+        if (other !is IProduto) return false
         if (codigo != other.codigo) return false
 
         return true
