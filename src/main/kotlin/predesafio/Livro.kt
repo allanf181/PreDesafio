81788236d151ea310413e889c7ffbafe207be856
manasomali <matheus.marques_96@hotmail.com>
2020-09-23 15:32:58 -0300

    contrutor da classe Livro e override no equals

--- src/main/kotlin/predesafio/Livro.kt
+++ src/main/kotlin/predesafio/Livro.kt
@@ -6,5 +6,11 @@ class Livro(var codigo: Int,
             var anoDeLancamento: Int,
             var quantidade: Int,
             var preco: Double) {
-    
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Livro) return false
+        if (codigo != other.codigo) return false
+
+        return true
+    }
 }
